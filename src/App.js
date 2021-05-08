8d4eb183fcb3888c2edc9fc87922806d235086c5
saiefelgebali <saiefelsayed@gmail.com>
2021-05-08 19:55:19 +0100

    REFACTOR: Main component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Main from "./components/Main/Main";
 import "./App.scss";
-import Feed from "./components/Feed/Feed";
 
 function App() {
 	return (
 		<div id='app'>
 			<Router>
 				<Navigation />
-				<Main>
-					<Switch>
-						<Route path='/home'>
-							<Feed />
-						</Route>
-					</Switch>
-				</Main>
+				<Main />
 			</Router>
 		</div>
 	);
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router";
+import Feed from "../Feed/Feed";
 import "./Main.scss";
 
-function Main({ children }) {
-	return <main className='main'>{children}</main>;
+function Main() {
+	// Handle routing
+	return (
+		<main className='main'>
+			<Switch>
+				<Route path='/home'>
+					<Feed />
+				</Route>
+			</Switch>
+		</main>
+	);
 }
 
 export default Main;
