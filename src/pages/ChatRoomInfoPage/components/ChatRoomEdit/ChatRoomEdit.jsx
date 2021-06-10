6d0cb2bbaef3608b26970132aa6b744aa06448aa
saiefelgebali <saiefelsayed@gmail.com>
2021-06-10 19:52:53 +0100

    ADDED: Members controls layout chatInfo

--- src/pages/ChatRoomInfoPage/components/ChatRoomEdit/ChatRoomEdit.jsx
+++ src/pages/ChatRoomInfoPage/components/ChatRoomEdit/ChatRoomEdit.jsx
@@ -1,10 +1,20 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import ChatRoomEditImage from "../ChatRoomEditImage/ChatRoomEditImage";
+import { ProfileContext } from "../../../../context/profileContext";
 import styles from "./ChatRoomEdit.module.scss";
 
 function ChatRoomEdit({ chatRoom }) {
+	const profileContext = useContext(ProfileContext);
+
+	function handleLeaveChat() {
+		// Make leave mutation
+		// Refresh chatRooms
+		// Route to home
+	}
+
 	const Members = () =>
 		chatRoom.members.map((member) => (
 			<Member key={member.username} member={member} />
@@ -18,14 +28,34 @@ function ChatRoomEdit({ chatRoom }) {
 		return <FontAwesomeIcon icon={faUser} />;
 	};
 
-	const Member = ({ member }) => (
-		<Link className={styles.member} to={`/profile/${member.username}`}>
-			<div className={styles.pfp}>
-				<MemberImage member={member} />
+	const Member = ({ member }) => {
+		return (
+			<Link className={styles.member} to={`/profile/${member.username}`}>
+				<div className={styles.info}>
+					<div className={styles.pfp}>
+						<MemberImage member={member} />
+					</div>
+					<div className={styles.username}>{member.username}</div>
+				</div>
+				<Controls member={member} />
+			</Link>
+		);
+	};
+
+	const Controls = ({ member }) => {
+		if (profileContext.username !== member.username) return null;
+
+		return (
+			<div className={styles.controls}>
+				<div
+					title='Leave chat'
+					className={styles.item}
+					onClick={handleLeaveChat}>
+					<FontAwesomeIcon icon={faSignOutAlt} />
+				</div>
 			</div>
-			<div className={styles.username}>{member.username}</div>
-		</Link>
-	);
+		);
+	};
 
 	return (
 		<>
